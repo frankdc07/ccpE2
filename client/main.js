71118552753907ade092d78242f41e6ef7076059
frankdc0 <frankdc.07@gmail.com>
2017-04-07 02:13:30 -0500

    c

--- client/main.js
+++ client/main.js
@@ -27,21 +27,12 @@ Template.map.onCreated(function() {
   GoogleMaps.ready('map', function(map) {
     self.autorun(function(){
       var marker;
-      var flightPlanCoordinates = [
-            {lat: 37.772, lng: -122.214},
-            {lat: 21.291, lng: -157.821},
-            {lat: -18.142, lng: 178.431},
-            {lat: -27.467, lng: 153.027}
-          ];
-      var flightPath = new google.maps.Polyline({
-            path: flightPlanCoordinates,
-            geodesic: true,
-            strokeColor: '#FF0000',
-            strokeOpacity: 1.0,
-            strokeWeight: 2
-          });
-      flightPath.setMap(map);
-    });
+      //var rutas = Rutas.find({});
+      Rutas.find().forEach( function(ruta) {
+        marker = new google.maps.Marker({
+        position: new google.maps.LatLng(ruta.LATITUD, ruta.LONGITUD),
+        map: map.instance
+      });
 
 
       // Create and move the marker when latLng changes.
