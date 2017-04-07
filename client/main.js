e87b575406a21d1cb8489c0a83cbc21ae42cbb14
frankdc0 <frankdc.07@gmail.com>
2017-04-06 21:41:24 -0500

    c

--- client/main.js
+++ client/main.js
@@ -26,29 +26,45 @@ Template.map.onCreated(function() {
 
   GoogleMaps.ready('map', function(map) {
     var marker;
+    var flightPlanCoordinates = [
+          {lat: 37.772, lng: -122.214},
+          {lat: 21.291, lng: -157.821},
+          {lat: -18.142, lng: 178.431},
+          {lat: -27.467, lng: 153.027}
+        ];
+        var flightPath = new google.maps.Polyline({
+          path: flightPlanCoordinates,
+          geodesic: true,
+          strokeColor: '#FF0000',
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        });
+
+        flightPath.setMap(map);
+
 
       // Create and move the marker when latLng changes.
-      self.autorun(function() {
-        var latLng = Geolocation.latLng();
-        if (! latLng)
-          return;
-
-        // If the marker doesn't yet exist, create it.
-        if (! marker) {
-          marker = new google.maps.Marker({
-            position: new google.maps.LatLng(latLng.lat, latLng.lng),
-            map: map.instance
-          });
-        }
-        // The marker already exists, so we'll just change its position.
-        else {
-          marker.setPosition(latLng);
-        }
-
-        // Center and zoom the map view onto the current position.
-        map.instance.setCenter(marker.getPosition());
-        map.instance.setZoom(MAP_ZOOM);
-      });
+      // self.autorun(function() {
+      //   var latLng = Geolocation.latLng();
+      //   if (! latLng)
+      //     return;
+      //
+      //   // If the marker doesn't yet exist, create it.
+      //   if (! marker) {
+      //     marker = new google.maps.Marker({
+      //       position: new google.maps.LatLng(latLng.lat, latLng.lng),
+      //       map: map.instance
+      //     });
+      //   }
+      //   // The marker already exists, so we'll just change its position.
+      //   else {
+      //     marker.setPosition(latLng);
+      //   }
+      //
+      //   // Center and zoom the map view onto the current position.
+      //   map.instance.setCenter(marker.getPosition());
+      //   map.instance.setZoom(MAP_ZOOM);
+      // });
     });
 });
 
@@ -62,7 +78,7 @@ Template.map.helpers({
       // Initialize the map once we have the latLng.
       if (GoogleMaps.loaded() && latLng) {
         return {
-          center: new google.maps.LatLng(latLng.lat, latLng.lng),
+          //center: new google.maps.LatLng(latLng.lat, latLng.lng),
           zoom: MAP_ZOOM
         };
       }
