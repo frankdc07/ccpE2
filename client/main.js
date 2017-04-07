66c94ed057c6d705150f9fa579cfd4ce5cda0542
frankdc0 <frankdc.07@gmail.com>
2017-04-06 19:56:58 -0500

    commit

--- client/main.js
+++ client/main.js
@@ -15,6 +15,21 @@ Template.body.helpers({
   rutas(){
     return Rutas.find({});
   },
+  geolocationError: function() {
+    var error = Geolocation.error();
+    return error && error.message;
+  },
+  mapOptions: function() {
+    var latLng = Geolocation.latLng();
+      // Initialize the map once we have the latLng.
+      if (GoogleMaps.loaded() && latLng) {
+        console.log('hola');
+        return {
+          center: new google.maps.LatLng(latLng.lat, latLng.lng),
+          zoom: MAP_ZOOM
+        };
+      }
+    }
 });
 
 
@@ -50,20 +65,6 @@ Template.map.onCreated(function() {
 });
 
 Template.map.helpers({
-  geolocationError: function() {
-    var error = Geolocation.error();
-    return error && error.message;
-  },
-  mapOptions: function() {
-    var latLng = Geolocation.latLng();
-      // Initialize the map once we have the latLng.
-      if (GoogleMaps.loaded() && latLng) {
-        return {
-          center: new google.maps.LatLng(latLng.lat, latLng.lng),
-          zoom: MAP_ZOOM
-        };
-      }
-    }
   });
 //
 // Template.ruta.onCreated(function helloOnCreated() {
